1bddc0afd6b475de9f90c0e1367135b712d8ee0f
mat-cor <mattia.cordioli01@universitadipavia.it>
2017-11-21 13:09:41 +0100

    Deleted write_file row

--- chemicaldatautils/exclude_compounds.py
+++ chemicaldatautils/exclude_compounds.py
@@ -15,8 +15,6 @@ def exclude_rares(min_freq, cids, smiles, names, formulas, terms, t2freq):
             formulas_out.append(f)
             terms_out.append(t_list_out)
 
-    # write_file(fname, cids_out, smiles_out, names_out, formulas_out, terms_out)
-
     return cids_out, smiles_out, names_out, formulas_out, terms_out
 
 
@@ -32,7 +30,5 @@ def exclude_duplicates(cids, smiles, names, formulas, terms):
         names_out.append(names[ind])
         formulas_out.append(formulas[ind])
         terms_out.append(terms[ind])
-
-    # write_file(fname, cids_out, smiles_out, names_out, formulas_out, terms_out)
 
     return cids_out, smiles_out, names_out, formulas_out, terms_out
