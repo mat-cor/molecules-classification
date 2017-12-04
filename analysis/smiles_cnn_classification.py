3edf2fa67076cd8601fb4ed4c46895e0853bc790
mat-cor <mattia.cordioli01@universitadipavia.it>
2017-12-04 13:18:57 +0100

    Add smiles CNN classification

---
+++ analysis/smiles_cnn_classification.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import numpy as np
+import tensorflow as tf
+
+from sklearn.model_selection import train_test_split
+
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Convolution1D, MaxPooling1D, GlobalMaxPooling1D, Dropout, Dense
+from keras.layers.embeddings import Embedding
+from keras.optimizers import SGD
+from keras import backend as K
+
+
+# The default Tensorflow behavior is to allocate memory on all the available GPUs, even if it runs only on the selected
+# one. To avoid it, only the free GPU (defined by cmd line input)
+gpu = str(sys.argv[1])
+os.environ["CUDA_VISIBLE_DEVICES"] = gpu
+config = tf.ConfigProto()
+config.gpu_options.allow_growth=True
+sess = tf.Session(config=config)
+K.set_session(sess)
+
+
+DATA_LOC = '../data/'
+
+smiles = np.load(DATA_LOC+'smiles.npy')
+y = np.load(DATA_LOC+'multi_labels.npy')
+
+t = Tokenizer(filters='', lower=False, char_level=True)
+
+t.fit_on_texts(smiles)
+seqs = t.texts_to_sequences(smiles)
+X = pad_sequences(seqs)
+
+# Split in train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+print('Number of training examples: ', X_train.shape[0])
+print('Number of test examples: ', X_test.shape[0])
+print('Multi-label classification, number of classes: ', y_train.shape[1])
+
+sequence_length = X.shape[1]
+vocabulary_size = len(t.word_index)
+n_class = y_train.shape[1]
+embedding_size = 32
+
+# Model
+model = Sequential()
+model.add(Embedding(output_dim=embedding_size, input_dim=vocabulary_size, input_length=sequence_length))
+model.add(Convolution1D(64, 3, activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Dropout(0.25))
+model.add(Convolution1D(64, 3, activation='relu'))
+model.add(GlobalMaxPooling1D())
+model.add(Dropout(0.25))
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.25))
+model.add(Dense(n_class, activation='sigmoid'))
+print(model.summary())
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
+model.fit(X_train, y_train, epochs=10, batch_size=32, verbose=1)
+score = model.evaluate(X_test, y_test, batch_size=32)
+print('Test score: ', model.metrics_names, score)
---
+++ preprocess/cnn_data_handler.py
@@ -0,0 +1,42 @@
+'''
+Exctract the list of SMILES from the dataset and convert the labels to "one-hot" representation ([0 0 1 0 0 .. 0 1 0])
+'''
+import os
+from preprocess.load_data import loadDataset
+import numpy as np
+
+DATA_LOC = '../data/'
+filepath = os.path.join(DATA_LOC, 'dataset.tab')
+
+
+def getSMILES(file):
+    _, smiles, _, _, _, _, _ = loadDataset(file)
+
+    return np.array(smiles)
+
+
+def get_labels(file):
+    _, _, _, _, terms, _, term_set = loadDataset(file)
+    term2id = {}
+    id2term = {}
+
+    for i, t in zip(range(len(term_set)), term_set):
+        term2id[t] = i
+        id2term[i] = t
+
+    labels = np.zeros((len(terms), len(term_set)), dtype=int)
+    for t_list, i in zip(terms, range(len(terms))):
+        for term in t_list:
+            j = term2id[term]
+            labels[i, j] = 1
+
+    return labels
+
+
+def main():
+    np.save(DATA_LOC+'smiles', getSMILES(filepath))
+    np.save(DATA_LOC+'multi_labels', get_labels(filepath))
+
+
+if __name__ == "__main__":
+    main()
