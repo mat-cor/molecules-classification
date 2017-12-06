e1051a74885158d00d061608a63ac8f42fe29a11
mat-cor <mattia.cordioli01@universitadipavia.it>
2017-12-06 16:42:47 +0100

    Refactor exclude_compounds.py

--- preprocess/exclude_compounds.py
+++ preprocess/exclude_compounds.py
@@ -1,13 +1,22 @@
-def exclude_rare(min_freq, cids, smiles, names, formulas, terms, t2freq):
-    cids_out, smiles_out, names_out, formulas_out, terms_out = [], [], [], [], []
+from preprocess.frequencies import termsFrequency
+from preprocess.compute_memberships import membershipMatrix
+
+
+def exclude_rare(min_freq, cids, smiles, names, formulas, terms, tset):
+    cids_out, smiles_out, names_out, formulas_out, terms_out, tset_out = [], [], [], [], [], []
+
+    # Compute the terms memberships matrix (in order to compute frequencies of the terms)
+    m, term_labels = membershipMatrix(tset, terms)
+    frequency = termsFrequency(m)
+    t2freq = dict()
+    for fr, te in zip(frequency, term_labels):
+        t2freq[te] = fr
 
     for c, s, n, f, t_list in zip(cids, smiles, names, formulas, terms):
         t_list_out = []
-
         for t in t_list:
             if t2freq[t] >= min_freq:
                 t_list_out.append(t)
-
         if len(t_list_out) > 0:
             cids_out.append(c)
             smiles_out.append(s)
@@ -15,13 +24,19 @@ def exclude_rare(min_freq, cids, smiles, names, formulas, terms, t2freq):
             formulas_out.append(f)
             terms_out.append(t_list_out)
 
-    return cids_out, smiles_out, names_out, formulas_out, terms_out
+        for t_list in terms_out:
+            for t in t_list:
+                tset_out.append(t)
+
+        tset_out = list(set(tset_out))
+
+    return cids_out, smiles_out, names_out, formulas_out, terms_out, tset_out
 
 
 def exclude_duplicate(cids, smiles, names, formulas, terms):
     '''Found a set of SMILES, it will save only the first element with each SMILES'''
     smiles_set = set(smiles)
-    cids_out, smiles_out, names_out, formulas_out, terms_out = [], [], [], [], []
+    cids_out, smiles_out, names_out, formulas_out, terms_out, tset_out = [], [], [], [], [], []
 
     for sm in smiles_set:
         ind = smiles.index(sm)
@@ -31,19 +46,30 @@ def exclude_duplicate(cids, smiles, names, formulas, terms):
         formulas_out.append(formulas[ind])
         terms_out.append(terms[ind])
 
-    return cids_out, smiles_out, names_out, formulas_out, terms_out
+    for t_list in terms_out:
+        for t in t_list:
+            tset_out.append(t)
+
+    tset_out = list(set(tset_out))
+
+    return cids_out, smiles_out, names_out, formulas_out, terms_out, tset_out
 
 
-def exclude_size(cids, smiles, names, formulas, terms, min_size, max_size):
-    cids_out, smiles_out, names_out, formulas_out, terms_out = [], [], [], [], []
+def exclude_size(cids, smiles, names, formulas, terms, tset, min_size, max_size):
+    cids_out, smiles_out, names_out, formulas_out, terms_out, tset_out = [], [], [], [], [], []
     
     for c, s, n, f, t_list in zip(cids, smiles, names, formulas, terms):
         if min_size < len(s) < max_size:
-            print(len(s))
             cids_out.append(c)
             smiles_out.append(s)
             names_out.append(n)
             formulas_out.append(f)
             terms_out.append(t_list)
+
+    for t_list in terms_out:
+        for t in t_list:
+            tset_out.append(t)
+
+    tset_out = list(set(tset_out))
             
-    return cids_out, smiles_out, names_out, formulas_out, terms_out
+    return cids_out, smiles_out, names_out, formulas_out, terms_out, tset_out
--- preprocess/preprocessing.py
+++ preprocess/preprocessing.py
@@ -9,39 +9,23 @@ path = '../data/'
 cids_raw, smiles_raw, names_raw, formulas_raw, terms_raw, treeids_raw, tset_raw = loadDataset(path + 'DatasetRaw.tab')
 
 # "Unique" the duplicated rows (rows with the same SMILES)
-cids_u, smiles_u, names_u, formulas_u, terms_u = exclude_duplicate(cids_raw, smiles_raw, names_raw, formulas_raw,
-                                                                   terms_raw)
+cids_u, smiles_u, names_u, formulas_u, terms_u, tset_u = exclude_duplicate(cids_raw, smiles_raw, names_raw, formulas_raw,
+                                                                           terms_raw)
+# Exclude the compounds with 10 < smiles_length < 400
+c1, s1, n1, f1, t1, tset1 = exclude_size(cids_u, smiles_u, names_u, formulas_u, terms_u, tset_u, 10, 400)
 
-tset_u = []
-for t_list in terms_u:
-    for t in t_list:
-        tset_u.append(t)
+# Exclude terms with frequency < 20
+c, s, n, f, t, tset = exclude_rare(20, cids_u, smiles_u, names_u, formulas_u, terms_u, tset_u)
+c2, s2, n2, f2, t2, tset2 = exclude_rare(20, c1, s1, n1, f1, t1, tset1)
 
-tset_u = list(set(tset_u))  # membershipMatrix requires a list as input
-
-# Compute the terms memberships matrix (in order to compute frequencies of the terms)
-m, term_labels = membershipMatrix(tset_u, terms_u)
-frequency = termsFrequency(m)
-
-# Exclude the terms with frequency <20. If a chemical has only terms with such frequency, than it is excluded
-term2freq = dict()
-for fr, te in zip(frequency, term_labels):
-    term2freq[te] = fr
-
-c, s, n, f, t = exclude_rare(20, cids_u, smiles_u, names_u, formulas_u, terms_u, term2freq)
-
-# Save the processesed dataset
+# Save the processed data
 # write_dataset(path+'dataset.tab', c, s, n, f, t)
+write_dataset(path+'dataset10_400.tab', c2, s2, n2, f2, t2)
 
-# Recompute and save the memberships matrix
+
+# Compute and save the memberships matrix
 # cids, smiles, names, formulas, terms, treeids, tset = loadDataset(path+'dataset.tab')
 # m2, term_labels2 = membershipMatrix(tset, terms)
 # saveMatrixTab(path+'memberships.tab', m2, term_labels2, cids)
 
-# Exclude the compounds with 10 < smiles_length < 400
-
-c2, s2, n2, f2, t2 = exclude_size(c, s, n, f, t, 10, 400)
-
-# Save the processed dataset
-write_dataset(path+'dataset10_400.tab', c2, s2, n2, f2, t2)
 
