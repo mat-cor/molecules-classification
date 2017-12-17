f41b785fc5ea4a68ac1a7f47c894e3b73a99f6bc
mat-cor <mattia.cordioli01@universitadipavia.it>
2017-12-17 13:13:52 +0100

    .

--- preprocess/preprocessing.py
+++ preprocess/preprocessing.py
@@ -12,7 +12,7 @@ cids_raw, smiles_raw, names_raw, formulas_raw, terms_raw, treeids_raw, tset_raw 
 cids_u, smiles_u, names_u, formulas_u, terms_u, tset_u = exclude_duplicate(cids_raw, smiles_raw, names_raw, formulas_raw,
                                                                            terms_raw)
 # Exclude the compounds with 10 < smiles_length < 400
-c1, s1, n1, f1, t1, tset1 = exclude_size(cids_u, smiles_u, names_u, formulas_u, terms_u, tset_u, 10, 400)
+c1, s1, n1, f1, t1, tset1 = exclude_size(cids_u, smiles_u, names_u, formulas_u, terms_u, tset_u, 30, 60)
 
 # Exclude terms with frequency < 20
 # c, s, n, f, t, tset = exclude_rare(20, cids_u, smiles_u, names_u, formulas_u, terms_u, tset_u)
@@ -20,7 +20,7 @@ c2, s2, n2, f2, t2, tset2 = exclude_rare(20, c1, s1, n1, f1, t1, tset1)
 
 # Save the processed data
 # write_dataset(path+'dataset.tab', c, s, n, f, t)
-write_dataset(path+'dataset10_400.tab', c2, s2, n2, f2, t2)
+write_dataset(path+'dataset30_60.tab', c2, s2, n2, f2, t2)
 
 # Compute and save the memberships matrix
 # cids, smiles, names, formulas, terms, treeids, tset = loadDataset(path+'dataset.tab')
