742b2adeba8f722916e38366dd068145d9e517bc
mat-cor <mattia.cordioli01@universitadipavia.it>
2018-01-14 18:11:52 +0100

    .

--- analysis/embedder_train.py
+++ analysis/embedder_train.py
@@ -35,18 +35,16 @@ K.set_session(sess)
 DATA_LOC = '../data/'
 with open(DATA_LOC+'termdict.pickle', 'rb') as handle:
     termdict = pickle.load(handle)
+with open(DATA_LOC+'smiles_vocabulary.pickle', 'rb') as handle:
+    vocabulary = pickle.load(handle)    
 smiles = np.load(DATA_LOC+'smiles.npy')
-t = Tokenizer(filters='', lower=False, char_level=True)
-t.fit_on_texts(smiles)
-seqs = t.texts_to_sequences(smiles)
+seqs = [[vocabulary[c] for c in list(s)] for s in smiles]
+
 X = pad_sequences(seqs, padding='post')
 y = np.load(DATA_LOC+'multi_labels.npy')
 
-print('Number of examples: ', X.shape[0])
-print('Multi-label classification, number of classes: ', y.shape[1])
-
 sequence_length = X.shape[1]
-vocabulary_size = len(t.word_index)
+vocabulary_size = len(vocabulary)
 n_class = y.shape[1]
 embedding_size = 64
 
@@ -54,8 +52,8 @@ embedding_size = 64
 model = Sequential()
 model.add(Embedding(output_dim=embedding_size, input_dim=vocabulary_size,
                     input_length=sequence_length))
-model.add(Convolution1D(32, 2, activation='relu'))
-model.add(MaxPooling1D(pool_size=2))
+model.add(Convolution1D(32, 3, activation='relu'))
+model.add(MaxPooling1D(pool_size=3))
 model.add(Convolution1D(32, 3, activation='relu'))
 model.add(MaxPooling1D(pool_size=3))
 model.add(Dropout(0.25))
@@ -64,7 +62,6 @@ model.add(Flatten())
 model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(n_class, activation='sigmoid'))
-
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
 model.fit(X, y, epochs=100, batch_size=64, verbose=1)
--- preprocess/smiles_voc.py
+++ preprocess/smiles_voc.py
@@ -22,7 +22,7 @@ print(len(char_set))
 voc = {}
 
 for i, c in enumerate(char_set):
-    voc[i+1] = c
+    voc[c] = i+1
 path = '../data/'
 with open(path+'smiles_vocabulary.pickle', 'wb') as handle:
     pickle.dump(voc, handle, protocol=pickle.HIGHEST_PROTOCOL)
