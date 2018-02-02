fc8d446f93227e7a338d2ebebbd70de18454c24b
mat-cor <mattia.cordioli01@universitadipavia.it>
2018-02-02 14:25:42 +0100

    Add fp_comparison

--- analysis/fp_comparison.py
+++ analysis/fp_comparison.py
@@ -17,12 +17,12 @@ from preprocess.smiles_embedder import get_cnn_fingerprint
 import tensorflow as tf
 from keras import backend as K
 
-gpu = str(sys.argv[1])
-os.environ["CUDA_VISIBLE_DEVICES"] = gpu
-config = tf.ConfigProto()
-config.gpu_options.allow_growth=True
-sess = tf.Session(config=config)
-K.set_session(sess)
+# gpu = str(sys.argv[1])
+# os.environ["CUDA_VISIBLE_DEVICES"] = gpu
+# config = tf.ConfigProto()
+# config.gpu_options.allow_growth=True
+# sess = tf.Session(config=config)
+# K.set_session(sess)
 
 path = '../data/'
 termdict = load_pickle(path+'termdict.pickle')
@@ -31,36 +31,37 @@ dataset = load_data(path+'dataset.csv')
 smiles = dataset['SMILES']
 labels = categorical_labels(dataset['Terms'], termdict)
 
-cnn_fp_data = get_cnn_fingerprint(smiles)
-ecfp_d = load_pickle('../data/ecfp-data.pickle')
-ecfp_l = load_pickle('../data/ecfp-labels.pickle')
-ecfp_data, ecfp_labels = [], []
-for cid in sorted(ecfp_d.keys()):
-    ecfp_data.append(ecfp_d[cid])
-    ecfp_labels.append(ecfp_l[cid])
+cnn_fp_data = get_cnn_fingerprint(smiles[:1000])
 
-ecfp_data = np.array(ecfp_data)
-ecfp_labels = np.array(ecfp_labels)
-
-with open('../results/LRauc.csv', 'w', newline='') as csvfile:
-    writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['Term', 'CNNFP', 'ECFP'])
-
-    k = 0
-    for t in termdict.keys():
-
-        k += 1
-        print(k)
-        print(datetime.datetime.now())
-
-        y = labels[:, termdict[t]]
-        y_ecfp = ecfp_labels[:, termdict[t]]
-
-        logreg = LogisticRegression()
-
-        auc_cnnfp = cross_val_score(logreg, cnn_fp_data, y, cv=10, scoring='roc_auc', n_jobs=-1)
-        auc_ecfp = cross_val_score(logreg, ecfp_data, y_ecfp, cv=10, scoring='roc_auc', n_jobs=-1)
-
-        writer.writerow([t, auc_cnnfp.mean(), auc_ecfp.mean()])
-        print(t, auc_cnnfp.mean(), auc_ecfp.mean())
-        print(datetime.datetime.now())
+# ecfp_d = load_pickle('../data/ecfp-data.pickle')
+# ecfp_l = load_pickle('../data/ecfp-labels.pickle')
+# ecfp_data, ecfp_labels = [], []
+# for cid in sorted(ecfp_d.keys()):
+#     ecfp_data.append(ecfp_d[cid])
+#     ecfp_labels.append(ecfp_l[cid])
+#
+# ecfp_data = np.array(ecfp_data)
+# ecfp_labels = np.array(ecfp_labels)
+#
+# with open('../results/LRauc.csv', 'w', newline='') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=';', quoting=csv.QUOTE_MINIMAL)
+#     writer.writerow(['Term', 'CNNFP', 'ECFP'])
+#
+#     k = 0
+#     for t in termdict.keys():
+#
+#         k += 1
+#         print(k)
+#         print(datetime.datetime.now())
+#
+#         y = labels[:, termdict[t]]
+#         y_ecfp = ecfp_labels[:, termdict[t]]
+#
+#         logreg = LogisticRegression()
+#
+#         auc_cnnfp = cross_val_score(logreg, cnn_fp_data, y, cv=10, scoring='roc_auc', n_jobs=-1)
+#         auc_ecfp = cross_val_score(logreg, ecfp_data, y_ecfp, cv=10, scoring='roc_auc', n_jobs=-1)
+#
+#         writer.writerow([t, auc_cnnfp.mean(), auc_ecfp.mean()])
+#         print(t, auc_cnnfp.mean(), auc_ecfp.mean())
+#         print(datetime.datetime.now())
